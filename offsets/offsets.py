34f8d2d10e1fe18d3c62c86442b465a26eb7b111
samgdotson <sgd2@illinois.edu>
2020-03-23 17:01:39 -0500

    adds functions to calculate the offsets

--- offsets/offsets.py
+++ offsets/offsets.py
@@ -7,20 +7,23 @@ def abbott_elec():
 
 	Returns:
 	--------
-	price : float
-		The price per kWh for electricity produced by APP
+	per_kwh : float
+		The price in dollars per kWh for electricity produced 
+		by APP
 	"""
-	pass
+	per_kwh = 0.08 # [$/kWh]
 
-def to_kwh(mdot):
+	return 
+
+def to_kwh(m):
 	"""
 	This function converts a mass flow rate in klbs/hr of steam
 	to an energy in kWh. Known values are currently hard coded. 
 	
 	Parameters:
 	-----------
-	mdot : float
-		This is the mass flow rate of Abbott steam in klbs/hr
+	m : float
+		This is the mass of Abbott steam in klbs/hr
 	
 	Returns:
 	--------
@@ -34,15 +37,105 @@ def to_kwh(mdot):
 	h_out = 1368 # outlet enthalpy [BTU/lb]
 
 	# times 0.29307107 to convert from BTU/hr to kilowatts
-	kwh = (mdot*(h_out-h_in))*0.29307107
+	kwh = (m*(h_out-h_in))*0.29307107
 	return kwh
 
 def abbott_steam():
-	pass
+	"""
+	This function returns the price per kwh thermal when that
+	power is used to produce steam at APP. 
 
-def solar_farm():
-	pass
+	Returns:
+	--------
+	per_kwh : float
+		The price per thermal kwh in dollars. 
+	"""
+
+	per_klb = 20 # dollars per klb of steam
+
+	kwh_eq = to_kwh(1) # kwh equivalent of steam
+
+	per_kwh = per_klb/kwh_eq
+	
+	return per_kwh
+
+def solar_ppa():
+	"""
+	This function returns the price per kwh of electricity
+	when it is produced by solar power at the UIUC solar farm.
+
+	Returns:
+	--------
+	per_kwh : float
+		The price per electric kWh in dollars. 
+	"""
+	per_kwh = 0.196 # [$/kWh] 
+
+	return per_kwh
 
 def wind_ppa():
-	pass
-	
+		"""
+	This function returns the price per kwh of electricity
+	when it is produced by solar power at the UIUC solar farm.
+
+	Returns:
+	--------
+	per_kwh : float
+		The price per electric kWh in dollars. 
+	"""
+	per_kwh = 0.0384 # [$/kWh]
+
+	return per_kwh
+
+
+def dollars_offset(capacity, cf, lifetime, use, replacing, eta=0.33):
+	"""
+	This function returns the cost of producing an amount of energy, 
+	equivalent to a nuclear reactor with given parameters, by some other
+	method.
+
+	Parameters:
+	-----------
+	capacity : float
+		The thermal capacity of the nuclear reactor in [kWth]
+	cf : float
+		The capacity factor of the nuclear reactor. [-] 
+	lifetime : integer
+		The energy producing lifetime of the nuclear reactor in [years]
+	use : string 
+		The desired end use of the energy produced by the nuclear reactor. 
+		Accepted values: 'electricity', 'steam', 'hydrogen'
+		Note: 'hydrogen' is not supported, yet. 
+	replacing : string
+		The proposed replacement for the energy produced by a reactor. 
+		Accepted values: 'solar', 'wind', 'abbott_th', 'abbott_e'
+		Where 'abbott_th' refers to thermal capacity from APP and 
+		'abbott_e' refers to electricity production. 
+
+	Returns: 
+	--------
+	offset : float
+		The number dollars required to produce an amount of energy
+		equivalent to some reactor by some other method.
+	"""
+
+	use_switcher = {
+	'electricity': capacity*cf*lifetime*eta, 
+	'steam':capacity*cf*lifetime,
+	'hydrogen': np.nan
+	}
+
+	energy_total = use_switcher[use]
+
+	alternative_switcher = {
+	'solar': solar_ppa(),
+	'wind': wind_ppa(),
+	'abbott_th': abbott_steam(),
+	'abbott_e': abbott_elec()
+	}
+
+	alt_cost = alternative_switcher[replacing]
+
+	offset = alt_cost*energy_total
+
+	return offset
